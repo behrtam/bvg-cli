417ab97d26086bcb2e8aa9762cef26852775291f
Tammo Behrends <behr.tam@gmail.com>
2015-08-31 17:21:20 +0200

    Add function to request departure times

--- bvg_cli.py
+++ bvg_cli.py
@@ -3,6 +3,7 @@ from lxml import html
 
 
 BVG_URL = 'http://mobil.bvg.de/Fahrinfo/bin/stboard.bin/dox?'
+# https://fahrinfo.bvg.de/Fahrinfo/bin/stboard.bin/dox?input=9079201&start=yes
 
 
 def request_station_ids(station_name):
@@ -39,3 +40,25 @@ def request_station_ids(station_name):
     station_name = tree.cssselect('span.desc strong')[0].text
     station_id = tree.cssselect('p.links a')[0].get('href').split('&')[1].split('=')[1]
     return (station_name, station_id), True
+
+
+def request_departures(station_id):
+    ''' Requests the departure times for the provided station id.
+
+    Return a tuple (data, ok). Data holdes the <departures> with time, line and
+    destination. The status flag can be True or False if there are network problems.
+    '''
+    payload = {'input' : station_id, 'start' : 'yes' }
+    r = requests.get(BVG_URL, params= payload)
+
+    # network
+    if r.status_code != 200:
+        return None, False
+
+    tree = html.fromstring(r.content)
+
+    data = []
+    for row in tree.cssselect('tbody tr'):
+        cells = tuple(e for e in row.text_content().split('\n') if e)
+        data.append(cells)
+    return data, True
