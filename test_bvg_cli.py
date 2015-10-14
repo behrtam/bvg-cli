936ed04e4f9efb6d6c6e2ea76833445a0ae9a01e
Tammo Behrends <behr.tam@gmail.com>
2015-10-14 17:48:38 +0200

    Add tests for filter types of transport

--- test_bvg_cli.py
+++ test_bvg_cli.py
@@ -1,7 +1,7 @@
 import httpretty
 
 from bvg_cli import BVG_URL
-from bvg_cli import request_departures, request_station_ids
+from bvg_cli import request_departures, request_station_ids, create_products_filter
 
 from html_dumps import DEPARTURE_HTML, STATION_HTML
 
@@ -53,3 +53,34 @@ def test_limit():
     departures, ok = request_departures('9120025', limit=2)
     assert ok is True
     assert len(departures) == 2
+
+
+@httpretty.activate
+def test_parameter_products_filter():
+    httpretty.register_uri(httpretty.GET, BVG_URL, body=DEPARTURE_HTML)
+    products_filter = create_products_filter(select='US')
+    _, ok = request_departures('any id', 2, products_filter)
+    request = httpretty.last_request()
+    assert hasattr(request, 'querystring')
+    assert 'productsFilter' in request.querystring
+    assert '11000000' in request.querystring['productsFilter']
+
+
+def test_base_products_filter():
+    assert create_products_filter() == '11111111'
+
+
+def test_select_products_filter():
+    assert create_products_filter(select='USR') == '11000100'
+
+
+def test_ignore_products_filter():
+    assert create_products_filter(ignore='US') == '00111111'
+
+
+def test_overvote_ignore_products_filter():
+    assert create_products_filter(select='US', ignore='SBT') == '11000000'
+
+
+def test_comma_products_filter():
+    assert create_products_filter(select='T,B,I') == '00111000'
